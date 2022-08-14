ea8216252754b674dfbbffc82e2a39202263d5cf
Caíque da Silva Machado <caique.machado@mercadolivre.com>
2022-08-14 11:03:18 -0300

    Renomeando pacote creational_builder para creationalBuilder

--- builder/creational_builder/creational_builder.go
+++ builder/creational_builder/creational_builder.go
@@ -1,4 +1,4 @@
-package creational_builder
+package creationalBuilder
 
 import (
   "fmt"
--- builder/main.go
+++ builder/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/CaiqueSM/Design-Parttners-in-GO/builder/creational_builder"
-
 func main() {
-	creational_builder.UseCaseHtmlBuilder()
+
 }
